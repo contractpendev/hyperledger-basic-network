51ae3516b2663e06bc5d64bb7308a94d547d0bc1
philip andrew <philip@contractpen.com>
2018-12-06 11:44:25 +0800

    ...

--- cli/createcard.sh
+++ cli/createcard.sh
@@ -4,7 +4,7 @@ echo $PRIV_KEYFILE
 PRIVATE_KEY=./../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/$PRIV_KEYFILE
 cd ../../../../../../../cli
 CERT=./../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem
-# Should the next line have the following network? -n net_basic to specify the network
+# Should the next line have the following network? -n helloworldstate to specify the network
 composer card create -p ./connection.json -u PeerAdmin -c "$CERT" -k "$PRIVATE_KEY" -r PeerAdmin -r ChannelAdmin --file ./PeerAdmin@hlfv12.card
 composer card import --file ./PeerAdmin@hlfv12.card 
 composer card list
--- cli/ping.sh
+++ cli/ping.sh
@@ -1,1 +1,2 @@
+composer identity list -c PeerAdmin@helloworldstate
 composer network ping --card PeerAdmin@helloworldstate
