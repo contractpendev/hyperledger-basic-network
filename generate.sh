0f4c93f9fe5c583bd503d923d2a7a09ba0ff6b69
contractpendev <philip@contractpen.com>
2018-12-20 20:30:05 +0800

    ...

--- generate.sh
+++ generate.sh
@@ -56,7 +56,7 @@ echo "${OUTPUT}"
 cd ../../../../../../
 cp .env_original data/$1/.env
 echo "COMPOSE_PROJECT_NAME={$1}\n" > data/$1/.env
-echo "FABRIC_SERVER_CERTIFICATE_FILE=${OUTPUT}\n" > data/$1/.env
+echo "FABRIC_SERVER_CERTIFICATE_FILE=${OUTPUT}\n" >> data/$1/.env
 
 cp docker-compose.yml data/$1
 
