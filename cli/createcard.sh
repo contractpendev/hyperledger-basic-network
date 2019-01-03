6c05f610485f65cb7ebff9a4916b19901c92b7ab
contractpendev <philip@contractpen.com>
2019-01-03 17:01:57 +0800

    test

--- cli/createcard.sh
+++ cli/createcard.sh
@@ -5,8 +5,8 @@ PRIVATE_KEY=./../crypto-config/peerOrganizations/org1.example.com/users/Admin@or
 cd ../../../../../../../cli
 CERT=./../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem
 # Should the next line have the following network? -n helloworldstate to specify the network
-composer card create -p ./connection.json -u PeerAdmin -c "$CERT" -k "$PRIVATE_KEY" -r PeerAdmin -r ChannelAdmin --file ./PeerAdmin@hlfv12.card
-composer card import --file ./PeerAdmin@hlfv12.card 
+composer card create -p ./connection.json -u PeerAdmin -c "$CERT" -k "$PRIVATE_KEY" -r PeerAdmin -r ChannelAdmin --file ./../crypto-config/PeerAdmin@hlfv12.card
+composer card import --file ./../crypto-config/PeerAdmin@hlfv12.card 
 composer card list
 
 
