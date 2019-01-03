a44294009bad804bbd4ce52c3705d3ff9cabd41f
contractpendev <philip@contractpen.com>
2019-01-03 18:38:07 +0800

    test

--- cli/startbna_test.sh
+++ cli/startbna_test.sh
@@ -1,5 +1,5 @@
 composer network install --archiveFile test.bna --card PeerAdmin@net_basic
-composer network start --card PeerAdmin@net_basic -n volumediscount -V 0.9.1 -A admin -S adminpw -f admin@volumediscount
-composer card import --file admin@volumediscount --card admin@volumediscount
-composer card import --file ./admin@volumediscount
+composer network start --card PeerAdmin@net_basic -n volumediscount -V 0.9.1 -A admin -S adminpw -f ./../crypto-config/admin@volumediscount
+composer card import --file ./../crypto-config/admin@volumediscount --card admin@volumediscount
+composer card import --file ./../crypto-config/admin@volumediscount
 composer card list
