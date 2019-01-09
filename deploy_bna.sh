ddb2d31b371386ce70788343a1fe0aa2b82b793b
contractpendev <philip@contractpen.com>
2019-01-09 16:36:07 +0800

    test

--- deploy_bna.sh
+++ deploy_bna.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Inputs are a) the hyperledger folders name which is name of docker running and
 # b) the bna file name
+# c) The bna file name without extension
 DATA_FOLDER="./data/$1/"
 BNA_FILE_PATH="./data/$1/bna/$2"
 DOCKER_COMMAND_IMAGE="$1.commandline"
@@ -12,3 +13,4 @@ echo "The bna file path relative to this path"
 echo "${BNA_FILE_PATH}"
 echo "The docker image to execute commands inside is"
 echo "${DOCKER_COMMAND_IMAGE}"
+docker exec ${DOCKER_COMMAND_IMAGE} ./cli/startbna.sh $3 $2
