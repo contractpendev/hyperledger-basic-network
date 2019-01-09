a67a9546f550d0cdbc6cb235b088ed9f07104ac5
contractpendev <philip@contractpen.com>
2019-01-09 21:16:42 +0800

    test

--- cli/startbna.sh
+++ cli/startbna.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# $1 is the BNA file name without file extension BNA at the end
-# $2 is the BNA file name with BNA at the end
-# Assume card exists at ./../crypto-config/PeerAdmin@hlfv12.card 
-composer network install --archiveFile ./../bna/$2 --card PeerAdmin@net_basic
-composer network start --card PeerAdmin@net_basic -n $1 -V 0.9.1 -A admin -S adminpw -f ./../crypto-config/admin@$1
+# $1 is logical name from package.json inside the bna file
+# $2 is the version from package.json inside the bna file
+# $3 is the BNA file name with BNA at the end
+composer network install --archiveFile ./../bna/$3 --card PeerAdmin@net_basic
+composer network start --card PeerAdmin@net_basic -n $1 -V $2 -A admin -S adminpw -f ./../crypto-config/admin@$1
 composer card import --file ./../crypto-config/admin@$1 --card admin@$1
-composer card import --file ./../crypto-config/admin@$1
 composer card list
+
