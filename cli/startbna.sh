d18e259e8620b020d004794fac0de30f554c4e26
contractpendev <philip@contractpen.com>
2019-01-10 13:05:21 +0800

    test

--- cli/startbna.sh
+++ cli/startbna.sh
@@ -4,7 +4,12 @@
 # $3 is the BNA file name with BNA at the end
 cd ..
 cd cli
+echo $1
+echo $2
+echo $3
+echo "Installing card"
 composer network install --archiveFile ./../bna/$3 --card PeerAdmin@net_basic
+echo "Starting"
 composer network start --card PeerAdmin@net_basic -n $1 -V $2 -A admin -S adminpw -f ./../crypto-config/admin@$1
 composer card import --file ./../crypto-config/admin@$1 --card admin@$1
 composer card list
