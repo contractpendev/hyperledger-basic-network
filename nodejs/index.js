976e3bcd87a9e4b7695ae4ddd77c7a7332cbd45e
Philip Andrew <philip@contractpen.com>
2019-01-31 10:30:23 +0800

    working state so far

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,8 @@
 //var utils = require('fabric-client/lib/utils.js');
 var Client = require('fabric-client');
 const fs = require('fs');
-
+const path = require('path');
+const Fabric_CA_Client = require('fabric-ca-client');
 
 function className(obj) {
   console.log(obj.constructor.toString());
@@ -14,32 +15,80 @@ function getAllMethods(object) {
 };
 
 async function main() {
-  var client = Client.loadFromConfig('../cli/connection.json');
-  var mspId = client.getMspid();
-  var channel = client.getChannel();
-  var peers = channel.getPeers();
-  var firstPeer = peers[0];
-  var p = firstPeer.getPeer();
 
-  dirPath = './../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/';
-  dir = fs.readdirSync(dirPath);
-  file = dir[0];
-  var privateKey = fs.readFileSync(dirPath + file, {encoding: 'utf8'});
-  // Refer https://stackoverflow.com/questions/51095303/how-to-set-cert-when-calling-createchannel-in-fabric-node-sdk
-  // Use the key from ../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore
-  var certificate = fs.readFileSync('./../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem', {encoding: 'utf8'});
-  client.setAdminSigningIdentity(privateKey, certificate, mspId);
-  //client.newTransaction(true);
-  var installedChaincodes = await client.queryInstalledChaincodes(p, true);
-  //console.log('chaincodes');
-  //console.log(installedChaincodes);
-  var chaincodes = installedChaincodes.getChaincodes();
-  for (var n = 0; n < chaincodes.length; n=n+1) {
-    var code = chaincodes[n];
-    console.log(code);
-    console.log(getAllMethods(code));
-    console.log(code.name + " " + code.version);
+  try {
+    var client = Client.loadFromConfig('../cli/connection.json');
+    var mspId = client.getMspid();
+    var channel = client.getChannel();
+    var peers = channel.getPeers();
+    var firstPeer = peers[0];
+    var p = firstPeer.getPeer();
+
+    const storePath = path.join(__dirname, 'hfc-key-store');
+    var stateStore = await Client.newDefaultKeyValueStore({ path: storePath });
+    client.setStateStore(stateStore);
+    const cryptoSuite = Client.newCryptoSuite();
+    const cryptoStore = Client.newCryptoKeyStore({ path: storePath });
+    cryptoSuite.setCryptoKeyStore(cryptoStore);
+    client.setCryptoSuite(cryptoSuite);
+
+    dirPath = './../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/';
+    dir = fs.readdirSync(dirPath);
+    file = dir[0];
+    var privateKey = fs.readFileSync(dirPath + file, {encoding: 'utf8'});
+    // Refer https://stackoverflow.com/questions/51095303/how-to-set-cert-when-calling-createchannel-in-fabric-node-sdk
+    // Use the key from ../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore
+    var certificate = fs.readFileSync('./../crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/Admin@org1.example.com-cert.pem', {encoding: 'utf8'});
+    client.setAdminSigningIdentity(privateKey, certificate, mspId);
+    const admin_user = await client._setUserFromConfig({username: 'admin', password: 'adminpw'});
+
+    var installedChaincodes = await client.queryInstalledChaincodes(p, true);
+    var chaincodes = installedChaincodes.getChaincodes();
+    for (var n = 0; n < chaincodes.length; n=n+1) {
+      var code = chaincodes[n];
+    }
+    console.log('user is enrolled: ' + admin_user.isEnrolled());
+ 
+    var txId = client.newTransactionID();
+    const contractId = 'arg1';
+    const requestJSON = {
+      name: '1'
+    };
+    const tx = {
+      fcn: 'executeSmartLegalContract',
+      args: [contractId, JSON.stringify(requestJSON)],
+    };
+    const request = Object.assign(tx, {
+      txId,
+      chaincodeId: 'helloworld'
+      //chainId: 'mychannel',
+    });
+    // send the transaction proposal to the peers
+    const r = await channel.sendTransactionProposal(request);
+    console.log(r);
+    
+  } catch (ex) {
+    console.log(ex);
   }
+
+
+
+
+  /*const contractId = 'MYCONTRACT';
+  const request = {
+    fcn: 'executeSmartLegalContract',
+    args: [contractId, JSON.stringify(requestJSON)],
+  };*/
+
+  /*const request = Object.assign(tx, {
+    txId,
+    chaincodeId: config.chaincodeId,
+    chainId: config.chainId,
+  });*/
+    // send the transaction proposal to the peers
+//  return channel.sendTransactionProposal(request);
+
+  
 }
 
 (async () => {
